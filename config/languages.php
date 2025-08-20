04fbc956cf19d77871f655835761a6d707552de0
Fromthestone <md@lab51.org>
2025-08-20 14:24:16 +0000

    added languages

---
+++ config/languages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Supported Locales
+    |--------------------------------------------------------------------------
+    |
+    | A list of all locales supported by the application. The first locale
+    | is considered the default/source language.
+    |
+    */
+    'supported' => [
+        'it', 
+        'en',
+        'fr',
+    ],
+];
